36de5efb75fabf4e1a44335fbc0ce6d3a5e096af
Debashree Dasgupta <debashree.dasgupta2013@srisriuniversity.edu.in>
2014-08-24 20:09:25 +0530

    Revert "Revert "edit""
    This reverts commit 3c3b3744afe6b1b4d3e7e617311aac238839c478.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,31 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {  ## set the value of the vector
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x    ## get the value of the vector
+  setsolve <- function(mean) m <<- solve  ## set the value of the mean
+  getsolve <- function() m  ## get the value of the mean
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {  
+    message("getting cached data")
+    return(m)  ## return the value if inverse already been calculated
+  }
+  data <- x$get()
+  m <- solve(data, ...) 
+  x$setsolve(m)
+  m  ## calculate the inverse if the value is not calculated before
 }
